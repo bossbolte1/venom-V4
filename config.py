99ae9ad64cd1dffe1388369eeaff186d46e0055c
bossbolte1 <88039423+bossbolte1@users.noreply.github.com>
2024-07-29 13:59:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7366273927:AAHSaF62y6JL5wyOXxQMVucgLrn7rJZsYJI")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Sjhdikwnsm")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1375408229")
 
 
 
